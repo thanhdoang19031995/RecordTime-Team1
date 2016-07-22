34d2355786c2631bb77179f9443e62b7a2feaa85
T138384 <luongtrungkien1294@gmail.com>
2016-07-22 10:33:59 +0700

    Update 22/7 ManageTime

--- src/managetime/Midlet.java
+++ src/managetime/Midlet.java
@@ -1,24 +1,28 @@
 package managetime;
 
 import ManagerTime.RecordTimeform;
+import ConnectWeb.Connect;
+import java.util.Vector;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.lcdui.*;
 
 public class Midlet extends MIDlet implements CommandListener {
-    
+
     Ticker newsTicker = new Ticker("Java J2ME");
     private Display display;
-    private final TextField userName;
-    private final TextField password;
+    private final TextField txtuserName;
+    private final TextField txtpassword;
     private final Form form;
     private final Command cancel;
     private final Command login;
     private final Command register;
-    private final Command recover ;
+    private final Command recover;
+    public static String uid;
 
     public Midlet() {
-        userName = new TextField("Email         ", "", 10, TextField.ANY);
-        password = new TextField("Password", "", 10, TextField.PASSWORD);
+        txtuserName = new TextField("Email         ", "", 30, TextField.ANY);
+        txtpassword = new TextField("Password", "", 30, TextField.PASSWORD);
+
         form = new Form("Log in");
         cancel = new Command("Cancel", Command.CANCEL, 2);
         login = new Command("Login", Command.OK, 2);
@@ -30,8 +34,8 @@ public class Midlet extends MIDlet implements CommandListener {
     public void startApp() {
         display = Display.getDisplay(this);
         form.setTicker(newsTicker);
-        form.append(userName);
-        form.append(password);
+        form.append(txtuserName);
+        form.append(txtpassword);
         form.addCommand(cancel);
         form.addCommand(login);
         form.addCommand(register);
@@ -46,37 +50,79 @@ public class Midlet extends MIDlet implements CommandListener {
     public void destroyApp(boolean unconditional) {
         notifyDestroyed();
     }
-
-    public void validateUser(String name, String password) {
-        if (name.equals("n") && password.equals("1")) {
-            menu();
-        } else {
-            tryAgain();
-        }
-    }
-
     public void menu() {
-        RecordTimeform f1 = new RecordTimeform("Record Time",Display.getDisplay(this).getCurrent());
-        //f1.setTitle(newsTicker);
+        RecordTimeform f1 = new RecordTimeform("Record Time", Display.getDisplay(this).getCurrent());
+        f1.setTicker(newsTicker);
         display = Display.getDisplay(this);
-        //f1.setDisplay(display);
         display.setCurrent(f1);
     }
 
     public void tryAgain() {
         Alert error = new Alert("Login Incorrect", "Please try again", null, AlertType.ERROR);
         error.setTimeout(Alert.FOREVER);
-        userName.setString("");
-        password.setString("");
+        txtuserName.setString("");
+        txtpassword.setString("");
         display.setCurrent(error, form);
     }
+    ConnectWeb.Connect User = new Connect();
 
     public void commandAction(Command c, Displayable d) {
-        String label = c.getLabel();
-        if (label.equals("Cancel")) {
+        //String label = c.getLabel();
+        if (c == cancel) {
             destroyApp(true);
-        } else if (label.equals("Login")) {
-            validateUser(userName.getString(), password.getString());
+        } else if (c == login) {
+            try {
+                String Email = txtuserName.getString();
+                String Password = txtpassword.getString();
+                String result = User.Login(Email, Password);
+                String[] parts = Split(result, ";");
+                String part1 = parts[0];
+                String part2 = parts[1];
+                int idUser = Integer.parseInt(part2.trim());
+                if (part1.equals("Ok")) {
+                    uid = "" + idUser;
+                    menu();
+                    System.out.println("SUCCESSFUL");
+                }
+            } catch (Exception e) {
+                Alert error = new Alert("Login Incorrect", "Please try again", null, AlertType.ERROR);
+                error.setTimeout(Alert.FOREVER);
+                txtuserName.setString("");
+                txtpassword.setString("");
+                display.setCurrent(error, form);
+                e.getMessage();
+                System.out.println("Fail");
+            }
+
         }
     }
+
+    public static String[] Split(String splitStr, String delimiter) {
+        StringBuffer token = new StringBuffer();
+        Vector tokens = new Vector();
+        // split
+        char[] chars = splitStr.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (delimiter.indexOf(chars[i]) != -1) {
+                // we bumbed into a delimiter
+                if (token.length() > 0) {
+                    tokens.addElement(token.toString());
+                    token.setLength(0);
+                }
+            } else {
+                token.append(chars[i]);
+            }
+        }
+        // don't forget the "tail"...
+        if (token.length() > 0) {
+            tokens.addElement(token.toString());
+        }
+        // convert the vector into an array
+        String[] splitArray = new String[tokens.size()];
+        for (int i = 0; i < splitArray.length; i++) {
+            splitArray[i] = (String) tokens.elementAt(i);
+        }
+        return splitArray;
+    }
+
 }
